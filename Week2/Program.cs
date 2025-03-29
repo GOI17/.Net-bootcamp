ada75c7c3d6a51061eb76ec9d2cfab75f69bac07
Gilberto Olivas <gilbertoolivas@Apex-Standard-PN2GJG69J1.local>
2025-03-29 17:46:22 -0600

    update modules names

---
+++ Week2/Program.cs
@@ -0,0 +1,11 @@
+﻿using HelloWorld;
+
+People p = new();
+p.Create(new("John", "Doe", 30));
+p.Create(new("John", "Doe", 30));
+p.Create(new("John", "Doe", 30));
+p.Create(new("John", "Doe", 30));
+p.Create(new("John", "Doe", 30));
+p.Create(new("John", "Doe", 30));
+/* p.Create(new("", "Doe", 30)); */
+p.DisplayPeople();
